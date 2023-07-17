8aa71a0f27efb8fb92475714a56a0bbb1e1327d2
Ivana <iv.cordasic@gmail.com>
2023-07-17 19:10:48 -0400

    Added methods to HomePage & RepairsPage

--- src/test/java/com/swirlingLeaves/pages/RepairsPage_IC.java
+++ src/test/java/com/swirlingLeaves/pages/RepairsPage_IC.java
@@ -1,13 +1,35 @@
 package com.swirlingLeaves.pages;
 
 import com.swirlingLeaves.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class RepairsPage_IC {
 
     public RepairsPage_IC(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//th[@class='o_list_record_selector']")
+    public WebElement repairReferenceCheckbox;
+
+    @FindBy(xpath = "//td[@class='o_list_record_selector']//input")
+    public List<WebElement> allCheckboxes;
+
+
+
+    public boolean verifyCheckboxIsSelected() {
+        for (WebElement each : allCheckboxes) {
+            if (!each.isSelected()){
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 
 
