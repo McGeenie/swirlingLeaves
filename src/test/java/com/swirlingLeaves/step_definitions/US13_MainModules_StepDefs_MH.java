da2a7e3f3f77c605756de7b712d9666356f14aa1
MHarjani <menakaharjani@gmail.com>
2023-07-18 12:50:49 -0400

    Updated feature file & created StepDefs

---
+++ src/test/java/com/swirlingLeaves/step_definitions/US13_MainModules_StepDefs_MH.java
@@ -0,0 +1,20 @@
+package com.swirlingLeaves.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class US13_MainModules_StepDefs_MH {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+    @When("user enters the POS Manager credentials")
+    public void user_enters_the_pos_manager_credentials() {
+
+    }
+    @Then("user should be able to see following modules")
+    public void user_should_be_able_to_see_following_modules() {
+
+    }
+}
