75909919eaa8e9e8c7cb15ae0d31746d9e2aeb1b
Sercan <hikmetsercanyilmaz@gmail.com>
2023-07-19 05:34:14 -0400

    US04 Calendar Step Definitions created

---
+++ src/test/java/com/swirlingLeaves/step_definitions/US04_Calendar_StepDefs_HY.java
@@ -0,0 +1,62 @@
+package com.swirlingLeaves.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US04_Calendar_StepDefs_HY {
+
+    @Given("User clicks on {string} module")
+    public void user_clicks_on_module(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User is on {string} page")
+    public void user_is_on_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User is on the {string} page")
+    public void user_is_on_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User clicks on {string} to enter event information")
+    public void user_clicks_on_to_enter_event_information(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should see the {string} pop-up")
+    public void user_should_see_the_pop_up(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User enters the event information in to the {string} input box")
+    public void user_enters_the_event_information_in_to_the_input_box(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User clicks {string} button on pop-up")
+    public void user_clicks_button_on_pop_up(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User sees the event information on calendar")
+    public void user_sees_the_event_information_on_calendar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks on the event summary")
+    public void user_clicks_on_the_event_summary() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should see the event information pop-up")
+    public void user_should_see_the_event_information_pop_up() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
