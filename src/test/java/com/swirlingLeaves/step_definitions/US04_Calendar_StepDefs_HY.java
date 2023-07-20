8dada7c03e259ee8ae2adb6a3588d65d3c6bc5d6
Sercan <hikmetsercanyilmaz@gmail.com>
2023-07-20 03:08:15 -0400

    US04 Calendar Step Definitions updated

--- src/test/java/com/swirlingLeaves/step_definitions/US04_Calendar_StepDefs_HY.java
+++ src/test/java/com/swirlingLeaves/step_definitions/US04_Calendar_StepDefs_HY.java
@@ -1,62 +1,94 @@
 package com.swirlingLeaves.step_definitions;
 
+import com.swirlingLeaves.pages.CalendarPage_HY;
+import com.swirlingLeaves.utilities.BrowserUtils;
+import com.swirlingLeaves.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class US04_Calendar_StepDefs_HY {
 
-    @Given("User clicks on {string} module")
-    public void user_clicks_on_module(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("User is on {string} page")
-    public void user_is_on_page(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    CalendarPage_HY calender = new CalendarPage_HY();
+
+
+    @Given("User clicks on {string}")
+    public void userClicksOn(String string) {
+
+
+        BrowserUtils.sleep(2);
+
+        calender.calendarModule.click();
+
     }
 
-    @When("User is on the {string} page")
-    public void user_is_on_the_page(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
+
+    @When("User is on {string} page")
+    public void user_is_on_page(String string) {
+
+        BrowserUtils.sleep(1);
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = String.valueOf(Driver.getDriver().getTitle().contains(string));
+
+        Assert.assertEquals(actualTitle,expectedTitle);
     }
+
+
     @Then("User clicks on {string} to enter event information")
     public void user_clicks_on_to_enter_event_information(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        BrowserUtils.sleep(1);
+
+        calender.timeBox.click();
+
     }
-    @Then("User should see the {string} pop-up")
-    public void user_should_see_the_pop_up(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
     @Then("User enters the event information in to the {string} input box")
     public void user_enters_the_event_information_in_to_the_input_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        BrowserUtils.sleep(1);
+
+        String createEventWindowHandle = Driver.getDriver().getWindowHandle();
+
+        Driver.getDriver().switchTo().window(createEventWindowHandle);
+
+        calender.eventSummaryInputBox.sendKeys("Delta1");
+
+
     }
     @Then("User clicks {string} button on pop-up")
     public void user_clicks_button_on_pop_up(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        calender.createButton.click();
+
     }
-    @Then("User sees the event information on calendar")
-    public void user_sees_the_event_information_on_calendar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
     @When("User clicks on the event summary")
     public void user_clicks_on_the_event_summary() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        calender.eventOnTimeBox.click();
+
+       String popUp = Driver.getDriver().getWindowHandle();
+
+       Driver.getDriver().switchTo().window(popUp);
+
     }
     @Then("User should see the event information pop-up")
     public void user_should_see_the_event_information_pop_up() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        BrowserUtils.sleep(1);
+
+        calender.popUpEventInformation.isDisplayed();
+
     }
+
 
 
 }
