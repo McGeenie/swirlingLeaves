0c557e90000a0847dfdbd18546ca61d990ed920c
McGeenie <89535011+McGeenie@users.noreply.github.com>
2023-07-25 20:41:33 -0400

    US01_G31_167 updated code

--- src/test/java/com/swirlingLeaves/runners/CukesRunner.java
+++ src/test/java/com/swirlingLeaves/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/swirlingLeaves/step_definitions",
         dryRun = false,
-        tags = "@US01Login",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
